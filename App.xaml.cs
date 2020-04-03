7dac622310522c8ad27f3edc344d13da7d870f65
david4neblio <43017430+david4neblio@users.noreply.github.com>
2020-04-03 17:58:15 -0400

    Added small changes to make compliant with Etherscan

--- App.xaml.cs
+++ App.xaml.cs
@@ -34,7 +34,7 @@ namespace NebliDex
 		//Mainnet version
 		public static int protocol_version = 10; //My protocol version
 		public static int protocol_min_version = 10; //Minimum accepting protocol version
-		public static string version_text = "v10.0.0";
+		public static string version_text = "v10.0.1";
 		public static bool run_headless = false; //If true, this software is ran in critical node mode without GUI on startup
 		public static int sqldatabase_version = 3;
 		public static int accountdat_version = 1; //The version of the account wallet
@@ -46,7 +46,9 @@ namespace NebliDex
 		//Allow for very small ERC20 order amounts (ideal for wBTC)
 		//Updated DAI contract to new version
 		//Fixed ETH confirmation bug
-		//Allow for new markets without protocol changes		
+		//Allow for new markets without protocol changes
+
+		//Added Etherscan API Key and introduced balance rotation for APIs		
 		
 		public static string App_Path = AppDomain.CurrentDomain.BaseDirectory;
 
